3f242e2b1ec3dbb57f25602476eba85b22b4ab9d
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2024-06-18 10:47:06 -0300

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.json({
-        hey: "guys", 
-        welcome: "back",
-        to: "another",
-        video: ",",
-        please: "subscribe!",
-    });
+  res.json({
+    hey: "guys",
+    welcome: "back",
+    to: "another",
+    video: ",",
+    please: "subscribe!",
+  });
 });
 
-app.listen(6969)
+app.listen(process.env.PORT || 6969, () => {
+  console.log(`Server is running on port ${process.env.PORT || 6969}`);
+});
